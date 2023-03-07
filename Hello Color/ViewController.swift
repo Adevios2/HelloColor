19d17ad7d72221cada7a037bb0ef6b969fc9b85a
Dostonbek <bmister595@gmail.com>
2023-03-07 17:03:06 -0500

    Initial Commit

---
+++ Hello Color/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Hello Color
+//
+//  Created by Dostonbek on 2/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var isPurple = false
+    var isYellow = false
+    var isGreen = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func changeColor(_ sender: Any) {
+        if isPurple {
+            view.backgroundColor = UIColor.blue
+            isPurple = false
+        } else {
+            view.backgroundColor = UIColor.purple
+            isPurple = true
+        }
+    }
+    
+}
